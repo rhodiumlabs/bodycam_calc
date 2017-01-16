ea64c982fb8ac16ee72d45f0133d2477061d492d
nadim <i.nadim@gmail.com>
2017-01-15 23:33:53 -0500

    updated styles and added tooltip & analytics

--- pages/container.js
+++ pages/container.js
@@ -10,7 +10,7 @@ export default class extends React.Component {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.1.0/milligram.min.css" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans|Work+Sans" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans|Work+Sans|Montserrat" rel="stylesheet" />
       </Head>
       <main className={"wrapper"}>
         {this.props.children}
@@ -18,3 +18,4 @@ export default class extends React.Component {
     </div>
   }
 }
+
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import React, {Component} from 'react'
 import style from 'next/css';
 import Container from './container';
+import ReactTooltip from 'react-tooltip'
 import _ from 'lodash';
+
+let mixpanel = {track: (a) => console.log(a)};
+if(typeof document !== 'undefined') {
+  mixpanel = require('mixpanel-browser');
+  mixpanel.init("2533434c0dfd027c72e10bb3909cfb2b");
+}
+
 
 const MANTA_RAY = 'MANTA_RAY';
 const STING_RAY = 'STING_RAY';
@@ -47,10 +55,15 @@ var formatter = new Intl.NumberFormat('en-US', {
 });
 
 
-
 export const bodyStyle = style({
+    marginTop: '4em',
     fontFamily: 'Work Sans',
     color: '#333',
+    ' h1': {
+      fontWeight: 'bold',
+      fontFamily: 'Montserrat',
+      color: 'rgb(26, 52, 69)'
+    },
     '& dt': {
       marginBottom: '0'
     },
@@ -65,36 +78,39 @@ export const bodyStyle = style({
 export const headerUnder = style({
     marginBottom: '1em',
     marginTop: '1em',
-    borderBottom: '1px dotted #9b4dca'
+    fontWeight: 'bold',
+    fontFamily: 'Montserrat',
+    color: '#e9c77b'
 });
 
 export const subHeader = style({
     fontSize: '14px',
 
-    color: '#9b4dca',
+    color: 'rgb(26, 52, 69)',
     margin: 0,
     marginBottom: '0.5em'
 });
 export const ssubHeader = style({
     fontSize: '12px',
     marginTop: '0em',
-    color: '#9b4dca',
+    color: 'rgb(26, 52, 69)',
     opacity: 0.8,
 });
-const priceStyle = style({
+export const priceStyle = style({
   float: 'right'
 })
-const receipt = style({
-  border: '1px dotted #9b4dca',
+export const receipt = style({
+  border: '1px dotted rgb(26, 52, 69)',
   padding: '1.5em',
+  position: 'fixed',
+  top: '4%',
+  right: '4%',
   '& dd': {
     fontSize: '12px'
   },
-  position: 'fixed',
-  right: '5%',
   width: '400px'
 })
-const optionStyle = style({
+export const optionStyle = style({
   cursor: 'pointer',
   fontSize: '11px',
   '& input': {
@@ -104,7 +120,7 @@ const optionStyle = style({
     background: '#EEE'
   }
 })
-const styleCamBox = style({
+export const styleCamBox = style({
     width: '40%',
     height: '200px',
     fontSize: '12px',
@@ -121,62 +137,82 @@ const styleCamBox = style({
 
 const CameraOption = (props) => {
   return (
-
     <dl style={{width: '100%'}}>
     <dt 
       className={styleCamBox} 
-      style={{border: props.camera == MANTA_RAY ? '2px solid #333' : '', backgroundImage: "url('/static/MANTA_RAY.png')"}} 
-      onClick={() => {props.selectCamera(MANTA_RAY)}}>
+      style={{border: props.camera == MANTA_RAY ? '2px solid rgb(26, 52, 69)' : '', backgroundImage: "url('/static/MANTA_RAY.png')"}} 
+      onClick={() => {props.selectCamera(MANTA_RAY); mixpanel.track("MANTA_RAY CLICKED");}}>
       Manta Ray
     </dt>
     <dt 
       className={styleCamBox} 
-      style={{border: props.camera == STING_RAY ? '2px solid #333' : '',backgroundImage: "url('/static/STING_RAY.png')"}} 
-      onClick={() => {props.selectCamera(STING_RAY)}}>
+      style={{border: props.camera == STING_RAY ? '2px solid rgb(26, 52, 69)' : '',backgroundImage: "url('/static/STING_RAY.png')"}} 
+      onClick={() => {props.selectCamera(STING_RAY); mixpanel.track("STING_RAY CLICKED");}}>
       Sting Ray
     </dt>
   </dl>);
 }
 
 const StingRayAdditions = (props) => {
-  const generateOption = (text, opt) => (
-    <dt className={optionStyle} onClick={(e) => props.selectOption(opt)} > 
+  const generateOption = (text, tooltip, opt) => (
+    <dt 
+      className={optionStyle} 
+      onClick={(e) => {
+        props.selectOption(opt); 
+        mixpanel.track(text + " CLICKED")
+      }}
+      data-tip={tooltip}
+      > 
       <input 
         type="checkbox" 
+        readOnly={true}
         checked={props.options[opt]} 
       /> {text}
     </dt>
   )
 
-  return (<div>
-    <h5 className={ssubHeader}>Sting Ray Features & Accessories</h5>
-    <dl>  
-      {generateOption('3G Option (for live streaming)', OPTION_STING_RAY_3G)}
-      {generateOption('GPS Option', OPTION_STING_RAY_GPS)}
-      {generateOption('Extra memory (+16GB)', OPTION_STING_RAY_MEMORY)}
-      {generateOption('Extra battery (+8hours recording time)', OPTION_STING_RAY_BATTERY)}
-      
-  </dl></div>)
+  return (
+    <div>
+      <h5 className={ssubHeader}>Sting Ray Features & Accessories</h5>
+      <dl>  
+        {generateOption('3G Option', 'This option allows SIM card to be added.<br /> The recorded video can be live-streamed to a remote device', OPTION_STING_RAY_3G)}
+        {generateOption('GPS Option', 'Videos can have a location embedded at the time of recording', OPTION_STING_RAY_GPS)}
+        {generateOption('Extra memory (+16GB)', '', OPTION_STING_RAY_MEMORY)}
+        {generateOption('Extra battery', '', OPTION_STING_RAY_BATTERY)}
+      </dl>
+    </div>
+  )
 }
 
 const DockingStation = (props) => {
-  const generateOption = (text, opt) => (
-    <dt className={optionStyle} onClick={(e) => props.onChange(opt)} > 
+  const generateOption = (text, tooltip, opt) => (
+    <dt 
+      className={optionStyle} 
+      onClick={(e) => {
+        props.onChange(opt); 
+        mixpanel.track(text + " CLICKED")}
+      } 
+      data-tip={tooltip}
+      > 
       <input 
         type="radio" 
+        readOnly={true}
         checked={props.option == opt} 
         
       /> {text}
     </dt>
   )
 
-  return (<div>
-    <h5 className={ssubHeader}>Docking Station</h5>
-    <dl>  
-      {generateOption('Simple dock', OPTION_DOCKING_STATION_SIMPLE)}
-      {generateOption('Advanced dock with local storage', OPTION_DOCKING_STATION_ADVANCED)}
-      {generateOption('None', OPTION_DOCKING_STATION_NONE)}
-  </dl></div>)
+  return (
+    <div>
+      <h5 className={ssubHeader}>Docking Station</h5>
+      <dl>  
+        {generateOption('Simple dock', 'Dock allows data transfer and charging. <br/> A windows PC is required to upload the video data.', OPTION_DOCKING_STATION_SIMPLE)}
+        {generateOption('Advanced dock with local storage', 'The advanced dock has local storage for <br/> automatic saving of video camera data. No extra PC required.', OPTION_DOCKING_STATION_ADVANCED)}
+        {generateOption('None','', OPTION_DOCKING_STATION_NONE)}
+      </dl>
+    </div>
+  )
 }
 
 const RadioOptions = (props) => (
@@ -192,9 +228,14 @@ const RadioOptions = (props) => (
               {props.options.map((op, i) => 
                 <dt className={optionStyle} 
                     style={{display: 'inline-block', marginLeft: '1em'}} 
-                    onClick={() => {props.onChange(op.value)}}>
+                    data-tip={op.tooltip}
+                    onClick={() => {
+                      props.onChange(op.value); 
+                      mixpanel.track(op.text + " CLICKED")
+                    }}>
                     <input 
                       type="radio" 
+                      readOnly={true}
                       checked={props.value == op.value}   
                     /> {op.text}
                 </dt>
@@ -211,22 +252,32 @@ export default class MainPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      camera: '',
+      init: true,
+      camera: MANTA_RAY,
       additionalFeatures: {},
       dockingStation: OPTION_DOCKING_STATION_NONE,
-      cameraQuantity: 0,
-      dailyHours: 0,
-      localSupport: 0,
-      retainingPeriod: 0,
+      cameraQuantity: 500,
+      dailyHours: 3,
+      localSupport: 1,
+      retainingPeriod: 12,
       options: {}
+    }; 
+    if(typeof document !== 'undefined') {
+      window.onscroll = (e) => {
+      }
     }
   }
+  componentWillUpdate() {
+    if(this.state.init) 
+      this.setState({init: false});
+  }
+
 	render() {
     const cameraCost = () => {
       let cost = 0;
       if(this.state.camera == STING_RAY) {
         cost += CAMERA_STING_RAY_COST;
-	_.each(this.state.additionalFeatures, ((val, ft) => { val ? cost += costMap[ft] : null}));      
+	      _.each(this.state.additionalFeatures, ((val, ft) => { val ? cost += costMap[ft] : null}));      
       }
 
       if(this.state.camera == MANTA_RAY) {
@@ -245,49 +296,55 @@ export default class MainPage extends Component {
     let yearlyMaintenanceFees = (totalRequiredStorage * PER_GIGABIT_COST * STORAGE_MULTIPLIER * 12);
     let yearlyMaintenanceFeesPerCam = this.state.cameraQuantity == 0 ? 0 : (yearlyMaintenanceFees / this.state.cameraQuantity);
     let recurringCosts = (this.state.localSupport == 1 ? YEARLY_SERVICE_COST(this.state.cameraQuantity) : 0) +  yearlyMaintenanceFees;
-    return <Container>
+    
+    return (
+      <Container>
+      <ReactTooltip multiline={true} place={'bottom'} effect={'solid'} />
       <div className={`${bodyStyle} container`}>
         <div className="row">
-          <div className="column"><h1>Body Cam Calculator</h1></div>
+          <div className="column"><h1>Bodycam Calculator</h1></div>
         </div>
-        <hr/>
          <div className="row">
-          <div className="column"><p style={{fontSize: '12px'}}>Welcome to rhodium's bodycam calculator. Below please choose your options in hardware, software and storage to see an estimated total</p>
-        </div>
+          <div className="column">
+            <p style={{fontSize: '12px'}}>
+              Welcome to rhodium's bodycam calculator. 
+              Below please choose your options in hardware, 
+              software and storage to see an estimated total
+            </p>
+          </div>
+          <div className="column"/>
         </div>
         
         <div className="row">
           <div className="column">
-            
-              <div className="row">
-                <h5 className={headerUnder}>Hardware Options</h5>
+            <div className="row">
+              <h5 className={headerUnder}>Hardware Options</h5>
+            </div>
+            <div className="row">
+              <div className="column">
+              <h5 className={subHeader}> 1. Choose your camera </h5>
               </div>
-              <div className="row">
-                <div className="column">
-                <h5 className={subHeader}> 1. Choose your camera </h5>
-                </div>
-              </div>
-              <div className="row">
-                <div className="column">
-                <CameraOption 
-                  camera={this.state.camera} 
-                  selectCamera={(camera) => this.setState({camera})}
-                />
-                </div>
-              </div>
-              
-              <div>
-
-              <RadioOptions
-                title='2. Number of cameras'
-                options={[
-                    {text:"Pilot (100 Units)", value: 100}, 
-                    {text:"Mid-Level (500 Units)", value: 500}, 
-                    {text:"Full-scale (1000 Units)", value: 1000}
-                ]}
-                value={this.state.cameraQuantity}
-                onChange={(cameraQuantity) => this.setState({cameraQuantity})}
+            </div>
+            <div className="row">
+              <div className="column">
+              <CameraOption 
+                camera={this.state.camera} 
+                selectCamera={(camera) => this.setState({camera})}
               />
+              </div>
+            </div>
+          <div>
+
+          <RadioOptions
+            title='2. Number of cameras'
+            options={[
+                {text:"Pilot (100 Units)", value: 100}, 
+                {text:"Mid-Level (500 Units)", value: 500}, 
+                {text:"Full-scale (1000 Units)", value: 1000}
+            ]}
+            value={this.state.cameraQuantity}
+            onChange={(cameraQuantity) => this.setState({cameraQuantity})}
+          />
                 <div className="row">
                   <div className="column"><h5 className={subHeader}> 3. Optional Features </h5></div>
                 </div>
@@ -299,20 +356,19 @@ export default class MainPage extends Component {
                   />
                 </div>
                 <div className="column">
-                  {this.state.camera == STING_RAY ? 
-                    <StingRayAdditions 
-                      options={this.state.additionalFeatures}
-                      selectOption={(opt) => 
-                        this.setState({additionalFeatures: Object.assign({}, this.state.additionalFeatures, {[opt]: !this.state.additionalFeatures[opt]})})}
-                    /> : null}
+                  <div style={{display: this.state.camera == STING_RAY ? 'block' : 'none'}}>
+                  <StingRayAdditions 
+                    options={this.state.additionalFeatures}
+                    selectOption={(opt) => 
+                      this.setState({additionalFeatures: Object.assign({}, this.state.additionalFeatures, {[opt]: !this.state.additionalFeatures[opt]})})}
+                  />
+                  </div>
                 </div>
                 </div>
               </div>
-
-
-                <div className="row">
-                  <h5 className={headerUnder}>Maintenance & Storage Options</h5>
-                </div>
+              <div className="row">
+                <h5 className={headerUnder}>Maintenance & Storage Options</h5>
+              </div>
               
               <RadioOptions
                 title='4. How frequent are the police officers going to use the cameras'
@@ -329,7 +385,7 @@ export default class MainPage extends Component {
                   options={[
                       {text:"Short Period (1 month) ", value: 1}, 
                       {text:"Medium Period (6 months)", value: 12},
-                      {text:"Long Period (24 months)", value: 24}
+                      {text:"Long Period (24 months +)", value: 24}
                   ]}
                   value={this.state.retainingPeriod}
                   onChange={(retainingPeriod) => this.setState({retainingPeriod})}
@@ -337,51 +393,58 @@ export default class MainPage extends Component {
               <RadioOptions
                 title={'6. Do you require local support and training?'}
                 options={[
-                    {text:"Yes", value: 1}, 
+                    {text:"Yes", tooltip: 'placeholder', value: 1}, 
                     {text:"No", value: 0}
                 ]}
                 value={this.state.localSupport}
                 onChange={(localSupport) => this.setState({localSupport})}
               />
           </div>
-          <div className="column">
+          <div className="column" ref={(receipt) => this.receipt = receipt}>
             {this.state.camera == '' ? <pre> Choose a camera </pre> : null}
-            {this.state.cameraQuantity == 0 ? <pre> Choose a camera quantity </pre> : null}
             <div className={receipt}>
+            {this.state.init ? 
+              <small style={{marginBottom: '10px', background: '#EEE', padding: '5px', display: 'block'}}>We've pre-selected our recommended options.</small> : null }
+            <br/>
             <dl>
               <dt>Hardware</dt>
-              
               <dd>{this.state.cameraQuantity || 0} x Camera Cost <span className={priceStyle}>{formatter.format(totalCameraCost)}</span> </dd>
               <dd>Per Camera Cost <span className={priceStyle}>{formatter.format(cameraCost())}</span> </dd>
               <dd>Docking Stations Cost <span className={priceStyle}>{formatter.format(dockingStationCost)}</span></dd>
-              
-              <dd style={{fontWeight: 'bold'}}>Total Hardware Cost <span className={priceStyle}>{formatter.format(totalHardwareCost)}</span></dd>
+              <dd>Total Hardware Cost <span className={priceStyle}>{formatter.format(totalHardwareCost)}</span></dd>
             </dl>
             <dl>
               <dt>Storage Cost</dt>
-              
               <dd>Maintenance Fee <span className={priceStyle}>{formatter.format(yearlyMaintenanceFeesPerCam)} / Camera / Year </span></dd>
             </dl>
             {this.state.localSupport == 1 ? 
+              <dl>
+                <dt>Support Cost</dt>
+                <dd>Training/Support <span className={priceStyle}>{formatter.format(YEARLY_SERVICE_COST(this.state.cameraQuantity))}/Year</span></dd>
+              </dl> : null 
+            }
             <dl>
-              <dt>Support Cost</dt>
-              
-              <dd>Training/Support <span className={priceStyle}>{formatter.format(YEARLY_SERVICE_COST(this.state.cameraQuantity))}/Year</span></dd>
-            </dl> : null }
-
-	    <dl>
-	      <dt>Total Cost</dt>
-
-	      <dd>Fixed Costs <span className={priceStyle}>{formatter.format(totalHardwareCost)}</span></dd>
+              <dt>Total Cost</dt>
+              <dd>Fixed Costs <span className={priceStyle}>{formatter.format(totalHardwareCost)}</span></dd>
               <dd>Recurring Costs <span className={priceStyle}>{formatter.format(recurringCosts)}/Year</span></dd>
-	    </dl>
-            <hr/>
-            <small>These prices are subject to change based on further specified requirements</small>
+            </dl>
+            <a className="button" href="mailto:hello@rhodium.io" style={{backgroundColor: 'rgb(26, 52, 69)'}}> Inquire more </a>
+            <small style={{marginBottom: '10px', background: '#EEE', fontSize: '10px', padding: '5px', display: 'block'}}>These prices are subject to change based on further specified requirements</small>
             </div>
           </div>
         </div>
-        
       </div>
+      
+      <footer style={{marginTop: '4em'}} className="container"> 
+      <hr/>
+        <div className="row">
+          <div className="column">
+             by <a href="http://rhodium.io" style={{fontWeight: 'bold', color: 'rgb(26, 52, 69)'}}>rhodium.io</a>
+          </div>
+        </div> 
+       
+      </footer>
       </Container>
+      )
   }
 }
