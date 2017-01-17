d0131db64beb5b7891dbff364579b47bde264d68
Nadim Islam <i.nadim@gmail.com>
2017-01-17 13:37:26 -0500

    Update container.js

--- pages/container.js
+++ pages/container.js
@@ -7,9 +7,9 @@ export default class extends React.Component {
       <Head>
         <title>BodyCam Cost Calculator</title>
         <meta charSet="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.1.0/milligram.min.css" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans|Work+Sans|Montserrat" rel="stylesheet" />
       </Head>
       <main className={"wrapper"}>
